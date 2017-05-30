847c1edc77331580a0268cae4451c63aeeeed342
Sauhard Pande <sauhardp@codeaurora.org>
2017-05-30 17:49:53 +0530

    sdm660: Replace libcamera_client to android.hardware.camera.common@1.0-helper
    libcamera_client is not a VNDK library and thus not available to
    vendors. These vendor libs were using the library due to CameraMetadata
    and CameraParamters classes. The classes are now available via
    .common@1.0-helper library which is available to vendors. Thus switching
    to the lib.
    
    Bug: 62523241
    Test: 2017 pixel devices builds and boots. Camera functionalities work.
    Change-Id: I16d0fcf94b54c480b8e817c63341fb9f45118867

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -130,13 +130,15 @@ LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
         $(call project-path-for,qcom-display)/libqservice
-LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libsync libgui
+LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync libgui
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
 LOCAL_SHARED_LIBRARIES += libcutils libdl
 ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
+
+LOCAL_STATIC_LIBRARIES := android.hardware.camera.common@1.0-helper
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
