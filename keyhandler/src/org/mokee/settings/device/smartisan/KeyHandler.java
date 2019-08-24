136803d919acf744ddd5c908e12c08159439b765
XiNGRZ <chenxingyu92@gmail.com>
2019-08-24 17:15:50 +0800

    sdm660: keyhandler: Migrate to new screenshot method
    Change-Id: Ie073160b2fa4144e84ac3cf8cdbc9ae6f97ca096

--- keyhandler/src/org/mokee/settings/device/smartisan/KeyHandler.java
+++ keyhandler/src/org/mokee/settings/device/smartisan/KeyHandler.java
@@ -21,6 +21,7 @@ import android.hardware.input.InputManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
+import android.os.RemoteException;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.InputDevice;
@@ -28,9 +29,9 @@ import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.ViewConfiguration;
 import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
 
 import com.android.internal.os.DeviceKeyHandler;
-import com.android.internal.util.ScreenshotHelper;
 
 import mokee.providers.MKSettings;
 
@@ -50,7 +51,6 @@ public class KeyHandler implements DeviceKeyHandler {
 
     private Context context;
     private PowerManager pm;
-    private ScreenshotHelper screenshotHelper;
 
     private int trustedShortcutsDeviceId = 0;
     private int trustedPowerDeviceId = 0;
@@ -82,7 +82,6 @@ public class KeyHandler implements DeviceKeyHandler {
     public KeyHandler(Context context) {
         this.context = context;
         this.pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        this.screenshotHelper = new ScreenshotHelper(context);
 
         try {
             shortcutsScanCode = Integer.parseInt(FileUtils.readOneLine(FILE_SHORTCUTS));
@@ -214,7 +213,11 @@ public class KeyHandler implements DeviceKeyHandler {
                 ? WindowManager.TAKE_SCREENSHOT_SELECTED_REGION
                 : WindowManager.TAKE_SCREENSHOT_FULLSCREEN;
 
-        screenshotHelper.takeScreenshot(type, true, true, handler);
+        try {
+            WindowManagerGlobal.getWindowManagerService().takeScreenshot(type);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error while trying to takeScreenshot.", e);
+        }
     }
 
 }
